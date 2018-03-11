034147d44599697369f05beace4be2811884dee4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-11 18:48:03 +0100

    :sparkles: Add user on callback

--- src/Application/Http/Handlers/AuthenticateHandler.php
+++ src/Application/Http/Handlers/AuthenticateHandler.php
@@ -5,6 +5,8 @@ namespace WouterDeSchuyter\DropParty\Application\Http\Handlers;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use WouterDeSchuyter\DropParty\Application\Oauth\DropboxOauthProvider;
+use WouterDeSchuyter\DropParty\Domain\Users\User;
+use WouterDeSchuyter\DropParty\Domain\Users\UserRepository;
 
 class AuthenticateHandler
 {
@@ -13,9 +15,15 @@ class AuthenticateHandler
      */
     private $dropboxOauthProvider;
 
-    public function __construct(DropboxOauthProvider $dropboxOauthProvider)
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(DropboxOauthProvider $dropboxOauthProvider, UserRepository $userRepository)
     {
         $this->dropboxOauthProvider = $dropboxOauthProvider;
+        $this->userRepository = $userRepository;
     }
 
     public function __invoke(Request $request, Response $response): Response
@@ -29,5 +37,15 @@ class AuthenticateHandler
         ]);
 
         $resourceOwner = $this->dropboxOauthProvider->getResourceOwner($accessToken);
+
+        $user = new User(
+            $resourceOwner->getId(),
+            $accessToken->getToken(),
+            $resourceOwner->toArray()['email'],
+            $resourceOwner->toArray()['name']['given_name'],
+            $resourceOwner->toArray()['name']['surname']
+        );
+
+        $this->userRepository->add($user);
     }
 }
