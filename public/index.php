e2c8fcde9eee662b4fbaa33de1971a75019dd436
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-11 12:45:24 +0100

    :bulb: Some documentation

--- public/index.php
+++ public/index.php
@@ -2,8 +2,11 @@
 
 use WouterDeSchuyter\DropParty\Application\Http\Application;
 
+// Define application directory once at entrypoint
 define('APP_DIR', dirname(__DIR__));
 
+// Load autoloader
 require_once APP_DIR . '/vendor/autoload.php';
 
+// Run application
 (new Application())->run();
