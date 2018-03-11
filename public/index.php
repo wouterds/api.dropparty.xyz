09eaba8c3c4cf64bdc9a021a269455868ab26386
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-11 15:20:46 +0100

    :sparkles: Configure debugger

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Tracy\Debugger;
 use WouterDeSchuyter\DropParty\Application\Http\Application;
 
 // Define application directory once at entrypoint
@@ -7,6 +8,12 @@ define('APP_DIR', dirname(__DIR__));
 
 // Load autoloader
 require_once APP_DIR . '/vendor/autoload.php';
+
+// Debugger
+$debuggerMode = getenv('APP_ENV') === 'production' ? Debugger::PRODUCTION : Debugger::DEVELOPMENT;
+Debugger::$showBar = $debuggerMode === Debugger::DEVELOPMENT;
+Debugger::$strictMode = $debuggerMode === Debugger::DEVELOPMENT;
+Debugger::enable($debuggerMode, APP_DIR . '/storage/logs');
 
 // Run application
 (new Application())->run();
