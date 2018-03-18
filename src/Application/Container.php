37f0cd28f19c71e4c08be779cb82ea62d5069255
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-18 12:18:51 +0100

    :sparkles: Filesystem

--- src/Application/Container.php
+++ src/Application/Container.php
@@ -9,6 +9,7 @@ use WouterDeSchuyter\DropParty\Application\Http\ServiceProvider as HttpServicePr
 use WouterDeSchuyter\DropParty\Application\Oauth\ServiceProvider as OauthServiceProvider;
 use WouterDeSchuyter\DropParty\Application\Users\ServiceProvider as UsersServiceProvider;
 use WouterDeSchuyter\DropParty\Infrastructure\Database\ServiceProvider as DatabaseServiceProvider;
+use WouterDeSchuyter\DropParty\Infrastructure\Filesystem\ServiceProvider as FilesystemServiceProvider;
 
 class Container extends LeagueContainer
 {
@@ -25,6 +26,7 @@ class Container extends LeagueContainer
         $container->addServiceProvider(OauthServiceProvider::class);
         $container->addServiceProvider(UsersServiceProvider::class);
         $container->addServiceProvider(DatabaseServiceProvider::class);
+        $container->addServiceProvider(FilesystemServiceProvider::class);
 
         return $container;
     }
---
+++ src/Infrastructure/Filesystem/ServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WouterDeSchuyter\DropParty\Infrastructure\Filesystem;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Flysystem\Filesystem;
+use Spatie\Dropbox\Client as DropboxClient;
+use Spatie\FlysystemDropbox\DropboxAdapter;
+use WouterDeSchuyter\DropParty\Domain\Users\AuthenticatedUser;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $provides = [
+        Filesystem::class,
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        $this->container->share(Filesystem::class, function() {
+            $authenticatedUser = $this->container->get(AuthenticatedUser::class);
+
+            $dropboxClient = new DropboxClient(
+                $authenticatedUser->getUser()->getDropboxAccessToken(),
+                null,
+                getenv('DROPBOX_MAX_UPLOAD_CHUNK_SIZE_MB') * 1024 ^ 2
+            );
+
+            $dropboxAdapter = new DropboxAdapter($dropboxClient);
+
+            return new Filesystem($dropboxAdapter);
+        });
+    }
+}
