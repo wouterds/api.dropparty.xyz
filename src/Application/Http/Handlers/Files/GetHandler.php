4346ba201e907791ef99124e0aa1b5cff850ffb3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-18 13:16:05 +0100

    :sparkles: Get file by id

--- src/Application/Http/Handlers/Files/GetHandler.php
+++ src/Application/Http/Handlers/Files/GetHandler.php
@@ -4,9 +4,12 @@ namespace WouterDeSchuyter\DropParty\Application\Http\Handlers\Files;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Http\Stream;
 use Teapot\StatusCode;
 use WouterDeSchuyter\DropParty\Domain\Files\FileId;
 use WouterDeSchuyter\DropParty\Domain\Files\FileRepository;
+use WouterDeSchuyter\DropParty\Domain\Users\UserRepository;
+use WouterDeSchuyter\DropParty\Infrastructure\Filesystem\Filesystem;
 
 class GetHandler
 {
@@ -16,11 +19,18 @@ class GetHandler
     private $fileRepository;
 
     /**
-     * @param FileRepository $fileRepository
+     * @var UserRepository
      */
-    public function __construct(FileRepository $fileRepository)
+    private $userRepository;
+
+    /**
+     * @param FileRepository $fileRepository
+     * @param UserRepository $userRepository
+     */
+    public function __construct(FileRepository $fileRepository, UserRepository $userRepository)
     {
         $this->fileRepository = $fileRepository;
+        $this->userRepository = $userRepository;
     }
 
     /**
@@ -30,14 +40,36 @@ class GetHandler
      */
     public function __invoke(Request $request, Response $response): Response
     {
-        if (empty($request->getParsedBodyParam('fileId'))) {
+        // No file id passed?
+        if (empty($request->getParam('id'))) {
             return $response->withStatus(StatusCode::NOT_FOUND);
         }
 
-        $fileId = new FileId($request->getParsedBodyParam('fileId'));
+        // Try to get file
+        $file = $this->fileRepository->getById(new FileId($request->getParam('id')));
 
-        $file = $this->fileRepository->getById($fileId);
+        // File not found?
+        if (empty($file)) {
+            return $response->withStatus(StatusCode::NOT_FOUND);
+        }
 
-        dump($file);exit;
+        // Get user
+        $user = $this->userRepository->getById($file->getUserId());
+
+        // Init filesystem for user
+        $filesystem = Filesystem::fromUser($user);
+
+        // Get resource
+        $resource = $filesystem->readStream($file->getPath());
+
+        // Create stream from resource
+        $stream = new Stream($resource);
+
+        // Build response
+        $response = $response->withBody($stream);
+        $response = $response->withHeader('Content-Length', $file->getSize());
+        $response = $response->withHeader('Content-Type', $file->getContentType());
+
+        return $response;
     }
 }
